23e86df44e1b6ab1cb98317e90295a5be6571ab4
Yavuz Ozdemir <islam.ozdemir41@gmail.com>
2023-12-28 22:24:50 +0300

    Create AccountsController.cs

---
+++ VbApi/Vb.Api/Controllers/AccountsController.cs
@@ -0,0 +1,65 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Vb.Data;
+using Vb.Data.Entity;
+
+namespace VbApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountsController : ControllerBase
+    {
+        private readonly VbDbContext dbContext;
+
+        public AccountsController(VbDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Account>> Get()
+        {
+            return await dbContext.Set<Account>().ToListAsync();
+        }
+
+        [HttpGet("{id}", Name = "Get")]
+        public async Task<Account> Get(int id)
+        {
+            var account = await dbContext.Set<Account>()
+                .Include(x => x.Customer)
+                .Include(x => x.AccountTransactions)
+                .Include(x => x.EftTransactions)
+                .SingleOrDefaultAsync(x => x.Id == id);
+            return account;
+        }
+
+        [HttpPost]
+        public async Task Post([FromBody] Account account)
+        {
+            await dbContext.Set<Account>().AddAsync(account);
+            await dbContext.SaveChangesAsync();
+        }
+
+        [HttpPut("{id}")]
+        public async Task Put(int id, [FromBody] Account account)
+        {
+            var fromdb = await dbContext.Set<Account>().SingleOrDefaultAsync(x => x.Id == id);
+            fromdb.AccountNumber = account.AccountNumber;
+            fromdb.IBAN = account.IBAN;
+            fromdb.Balance = account.Balance;
+            fromdb.CurrencyType = account.CurrencyType;
+            fromdb.Name = account.Name;
+            fromdb.OpenDate = account.OpenDate;
+
+            await dbContext.SaveChangesAsync();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task Delete(int id)
+        {
+            var fromdb = await dbContext.Set<Account>().SingleOrDefaultAsync(x => x.Id == id);
+            fromdb.IsActive = false;
+            await dbContext.SaveChangesAsync();
+        }
+    }
+}
