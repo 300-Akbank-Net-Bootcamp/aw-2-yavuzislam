cca1e826cbb6baaac18c7f1cbe780cab769a870e
Yavuz Ozdemir <islam.ozdemir41@gmail.com>
2023-12-28 22:24:59 +0300

    Create AddressesController.cs

---
+++ VbApi/Vb.Api/Controllers/AddressesController.cs
@@ -0,0 +1,63 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Vb.Data;
+using Vb.Data.Entity;
+
+namespace VbApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AddressesController : ControllerBase
+    {
+        private readonly VbDbContext dbContext;
+
+        public AddressesController(VbDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Address>> Get()
+        {
+            return await dbContext.Set<Address>().ToListAsync();
+        }
+
+        [HttpGet("{id}", Name = "Get")]
+        public async Task<Address> Get(int id)
+        {
+            var address = await dbContext.Set<Address>()
+                .Include(x=>x.Customer)
+                .SingleOrDefaultAsync(x => x.Id == id);
+            return address;
+        }
+
+        [HttpPost]
+        public async Task Post([FromBody] Address address)
+        {
+            await dbContext.Set<Address>().AddAsync(address);
+            await dbContext.SaveChangesAsync();
+        }
+
+        [HttpPut("{id}")]
+        public async Task Put(int id, [FromBody] Address address)
+        {
+            var fromdb = await dbContext.Set<Address>().SingleOrDefaultAsync(x=>x.Id == id);
+            fromdb.Address1 = address.Address1;
+            fromdb.Address2 = address.Address2;
+            fromdb.City = address.City;
+            fromdb.Country = address.Country;
+            fromdb.County = address.County;
+            fromdb.PostalCode = address.PostalCode;
+            
+            await dbContext.SaveChangesAsync();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task Delete(int id)
+        {
+            var fromdb=await dbContext.Set<Address>().SingleOrDefaultAsync(x=>x.Id == id);
+            fromdb.IsActive = false;
+            await dbContext.SaveChangesAsync();
+        }
+    }
+}
