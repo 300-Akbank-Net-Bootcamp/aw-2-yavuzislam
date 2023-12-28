cec426f2c35140147e5304c57da21fe06d962700
Yavuz Ozdemir <islam.ozdemir41@gmail.com>
2023-12-28 22:24:54 +0300

    Create AccountTransactionsController.cs

---
+++ VbApi/Vb.Api/Controllers/AccountTransactionsController.cs
@@ -0,0 +1,62 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Vb.Data;
+using Vb.Data.Entity;
+
+namespace VbApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountTransactionsController : ControllerBase
+    {
+        private readonly VbDbContext dbContext;
+
+        public AccountTransactionsController(VbDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<AccountTransaction>> Get()
+        {
+            return await dbContext.Set<AccountTransaction>().ToListAsync();
+        }
+
+        [HttpGet("{id}", Name = "Get")]
+        public async Task<AccountTransaction> Get(int id)
+        {
+            var accountTransaction = await dbContext.Set<AccountTransaction>()
+                .Include(x => x.Account)
+                .SingleOrDefaultAsync(x => x.Id == id);
+            return accountTransaction;
+        }
+
+        [HttpPost]
+        public async Task Post([FromBody] AccountTransaction accountTransaction)
+        {
+            await dbContext.Set<AccountTransaction>().AddAsync(accountTransaction);
+            await dbContext.SaveChangesAsync();
+        }
+
+        [HttpPut("{id}")]
+        public async Task Put(int id, [FromBody] AccountTransaction accountTransaction)
+        {
+            var fromdb = await dbContext.Set<AccountTransaction>().SingleOrDefaultAsync(x => x.Id == id);
+            fromdb.ReferenceNumber = accountTransaction.ReferenceNumber;
+            fromdb.TransactionDate = accountTransaction.TransactionDate;
+            fromdb.Amount = accountTransaction.Amount;
+            fromdb.Description = accountTransaction.Description;
+            fromdb.TransferType = accountTransaction.TransferType;
+
+            await dbContext.SaveChangesAsync();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task Delete(int id)
+        {
+            var fromdb = await dbContext.Set<AccountTransaction>().SingleOrDefaultAsync(x => x.Id == id);
+            fromdb.IsActive = false;
+            await dbContext.SaveChangesAsync();
+        }
+    }
+}
